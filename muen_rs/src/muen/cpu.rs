ef5f1ad0757049515d50779c950f1df7adbc2380
Lana Black <lana@illuminati.industries>
2025-07-01 14:46:27 +0000

    Init TSS

--- muen_rs/src/muen/cpu.rs
+++ muen_rs/src/muen/cpu.rs
@@ -7,21 +7,27 @@ static mut INTR_STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 static mut TRAP_STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
 static mut NMI_STACK:  [u8; STACK_SIZE] = [0; STACK_SIZE];
 
+#[allow(static_mut_refs, unsafe_op_in_unsafe_fn)]
+unsafe fn stack_to_virt(s: &[u8]) -> VirtAddr {
+    let p = s.as_ptr().offset(STACK_SIZE as isize).addr() as u64;
+    VirtAddr::new(p)
+}
+
+static mut TSS_LO: TaskStateSegment = TaskStateSegment::new();
 static mut GDT: GlobalDescriptorTable = GlobalDescriptorTable::new();
 
-const GDT_TSS_LO: usize = 3;
-const GDT_TSS_HI: usize = 4;
+//const GDT_TSS_LO: usize = 3;
+//const GDT_TSS_HI: usize = 4;
 
 #[allow(static_mut_refs, unsafe_op_in_unsafe_fn)]
 pub unsafe fn cpu_init() {
-    // GDT
     GDT.append(Descriptor::kernel_code_segment());
     GDT.append(Descriptor::kernel_data_segment());
-    GDT.load_unsafe();
-    // GDT
 
-    let mut tss_lo = TaskStateSegment::new();
-    tss_lo.interrupt_stack_table[0] = VirtAddr::new(INTR_STACK.as_mut_ptr().offset(STACK_SIZE as isize).addr() as u64);
-    tss_lo.interrupt_stack_table[1] = VirtAddr::new(TRAP_STACK.as_mut_ptr().offset(STACK_SIZE as isize).addr() as u64);
-    tss_lo.interrupt_stack_table[2] = VirtAddr::new( NMI_STACK.as_mut_ptr().offset(STACK_SIZE as isize).addr() as u64);
+    TSS_LO.interrupt_stack_table[0] = stack_to_virt(&INTR_STACK);
+    TSS_LO.interrupt_stack_table[1] = stack_to_virt(&TRAP_STACK);
+    TSS_LO.interrupt_stack_table[2] = stack_to_virt(& NMI_STACK);
+
+    GDT.append(Descriptor::tss_segment(&TSS_LO));
+    GDT.load();
 }
